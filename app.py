fad6d4720edb2f94ae9eaa5ae89f32aca38e896f
srtARES <163037741+srtARES@users.noreply.github.com>
2024-03-14 18:42:29 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 import psycopg2
 import psycopg2.extras
 
 app = Flask(__name__)
+CORS(app)
 
 def fetch_weather_data():
     try:
         conn = psycopg2.connect("dbname=Assignment user=postgres password=SrtAres1904")
-        # Use a dict cursor to get data in a dictionary format
         cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
         cursor.execute("SELECT * FROM open_weather ORDER BY ID DESC")
-        # Fetch all rows from the table
         data = cursor.fetchall()
-        # Convert data into a list of dictionaries for JSON serialization
         weather_data = [dict(row) for row in data]
         cursor.close()
         conn.close()
@@ -24,5 +23,5 @@ def fetch_weather_data():
 @app.route('/weather', methods=['GET'])
 def get_weather_data():
     weather_data = fetch_weather_data()
-    # Return the list of dictionaries as JSON
     return jsonify(weather_data)
+
